345a2f0fe20eca22437e89f4094cee9e1894a52d
Thomas Wong <thomas@wonghoyin-2.local>
2023-12-08 18:33:19 -0500

    update

--- back_end_api.py
+++ back_end_api.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+import random
 
 import joblib
 
@@ -72,31 +73,51 @@ class BackEnd:
     
     def modelling(self):
         df_clean = self.preprocessing(self.df)
-        
+    
         X, y = df_clean.iloc[:, :-1], df_clean.iloc[:, -1]
         X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, random_state=24, stratify=y)
         X_train, X_val, y_train, y_val  = train_test_split(X_train_full, y_train_full, test_size=0.2, random_state=24, stratify=y_train_full)
+
+        best_model = None
+        best_accuracy = 0
+        n_iterations = 10  # Number of iterations for random search
+
+        for _ in range(n_iterations):
+            # Randomly choose hyperparameters
+            units1 = random.choice([50, 100, 200, 300])
+            units2 = random.choice([50, 100, 150])
+            dropout_rate = random.choice([0.1, 0.2, 0.3])
+            learning_rate = random.choice([0.01, 0.001, 0.0001])
+
+            model = tf.keras.models.Sequential([
+                tf.keras.layers.Dense(units1, activation='relu'),
+                tf.keras.layers.Dense(units2, activation='relu'),
+                tf.keras.layers.Dropout(dropout_rate),
+                tf.keras.layers.Dense(80, activation='relu'),
+                tf.keras.layers.Dense(20, activation='relu'),
+                tf.keras.layers.Dense(20, activation='relu'),
+                tf.keras.layers.Dense(1, activation='sigmoid')
+            ])
+
+            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                      loss=tf.keras.losses.BinaryCrossentropy(),
+                      metrics=['accuracy'])
+
+            model.fit(X_train, y_train, batch_size=32, validation_data=(X_val, y_val), epochs=10)  # Reduced epochs for faster iteration
+
+            accuracy = model.evaluate(X_val, y_val)[1]
+            print(f"Iteration accuracy: {accuracy}, Parameters: units1={units1}, units2={units2}, dropout_rate={dropout_rate}, learning_rate={learning_rate}")
+
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_model = model
+
+        # Save the best model
+        joblib.dump(best_model, 'nn_model.joblib', compress=9)
+    
+        # Evaluate the best model
+        print("Best model performance on test set:", best_model.evaluate(X_test, y_test)[1])
         
-        model = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(300, activation='relu'),
-            tf.keras.layers.Dense(150, activation='relu'),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(80, activation='relu'),
-            tf.keras.layers.Dense(20, activation='relu'),
-            tf.keras.layers.Dense(20, activation='relu'),
-            tf.keras.layers.Dense(1, activation='sigmoid')
-        ])
-        
-        loss_fn = tf.keras.losses.BinaryCrossentropy()
-        optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.0001)
-        
-        model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
-        
-        model.fit(X_train, y_train, batch_size=32, validation_data=(X_val, y_val), epochs=100)
-        
-        joblib.dump(model, 'nn_model.joblib', compress=9)
-        
-        print(model.evaluate(X_test, y_test)[1])
         
 test = BackEnd()
 test.modelling()
